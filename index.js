1d16db2dcc8a24fb8867623b04979ceda8d5f2c9
NavyBlueSky <lylebang9397@gmail.com>
2023-08-22 20:34:59 +0900

    multer aws

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+const aws = require('aws-sdk'); //aws 설정을 위한 모듈
+const multerS3 = require('multer-s3'); //aws s3에 업로드하기 위한 multer설정
 const app = express();
 const PORT = 8000;
+
+//aws 설정
+aws.config,update({
+    accessKeyId: 'AKIAWF4OHA5JPWCZVB5A',
+    secretAccessKey: "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",
+    region: 'ap-northeast-2'
+})
+
+//aws s3 인스턴스 생성
+const s3 = new aws.S3();
 
 //view engine 설정
 app.set('view engine', 'ejs');
 //정적파일세팅
 app.use('/uploads', express.static(__dirname + '/uploads'));
-//multer 설정
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },
-    filename: (req, file, cb) => {
-        //확장자 분리
-        const ext = path.extname(file.originalname);
-        cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
-    },
+
+
+// //multer 설정
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'uploads/');
+//     },
+//     filename: (req, file, cb) => {
+//         //확장자 분리
+//         const ext = path.extname(file.originalname);
+//         cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+//     },
+// });
+// const upload = multer({ storage });
+
+//multer설정 aws
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'test-server-multer-si',
+        acl: 'public-read', // 파일접근권한 (public-read로 해야 업로드된 파일이 공개)
+        metadata: function(req, file, cb) {
+            cb(null, {fileName: file.filename});
+        },
+        key: function(req, file, cb) {
+            cb(null, Date.now().toString() + '-' + file.originalname);
+        },
+    }),
 });
-const upload = multer({ storage });
+
 
 //router
 app.get('/', (req, res) => {
